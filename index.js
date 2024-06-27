663c9ef8a6ea8ac9832f3de35cb7deb7fc71b308
Error7799 <kingy7799sentamu@gmail.com>
2024-06-27 21:20:37 +0300

    Done :)

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = "Savio";
+let welcomeMessage = "Welcome to Flatbook, Savio !"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, SAVIO !"
+let shortGreeting = "Welcome, S!"
